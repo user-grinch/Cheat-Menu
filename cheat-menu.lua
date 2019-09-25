101f0e1399b9b75d7c60701b61df737a3a0b42f7
Grinch_ <user.grinch@gmail.com>
2019-09-24 19:08:16 -0700

    Changed version from staging to master

--- cheat-menu.lua
+++ cheat-menu.lua
@@ -20,7 +20,7 @@ script_description("Cheat Menu for Grand Theft Auto San Andreas")
 script_url("https://forum.mixmods.com.br/f5-scripts-codigos/t1777-lua-cheat-menu")
 script_dependencies("ffi","mimgui","memory","MoonAdditions")
 script_properties('work-in-pause')
-script_version("1.7-staging")
+script_version("1.7")
 script_version_number(24092019) -- DDMMYYYY
 
 -- All the command keys used throughout the Cheat-Menu
