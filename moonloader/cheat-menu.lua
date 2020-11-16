c87590ce3e1b2d85394848279fd3ebfcc7c70b23
Grinch_ <user.grinch@gmail.com>
2020-11-17 02:43:33 +0600

    Release prep

--- moonloader/cheat-menu.lua
+++ moonloader/cheat-menu.lua
@@ -20,8 +20,8 @@ script_description("Cheat Menu for Grand Theft Auto San Andreas")
 script_url("https://forum.mixmods.com.br/f5-scripts-codigos/t1777-moon-cheat-menu")
 script_dependencies("ffi","lfs","memory","mimgui","MoonAdditions")
 script_properties('work-in-pause')
-script_version("2.2-beta")
-script_version_number(2020111501) -- YYYYMMDDNN
+script_version("2.2")
+script_version_number(2020111701) -- YYYYMMDDNN
 
 print(string.format("Loading v%s (%d)",script.this.version,script.this.version_num))
 
