f7d11f2d73363d2dad1d46cb68885dda60b9ac99
Grinch_ <user.grinch@gmail.com>
2021-12-04 19:42:31 +0600

    Add more contrast to menu colors

--- tools/DebugVC.bat
+++ tools/DebugVC.bat
@@ -2,6 +2,6 @@
 cd tools
 premake5.exe vs2022
 cd ../build
-call "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 MsBuild CheatMenu.sln /property:Configuration=Debug /t:CheatMenuVC
 cd ..
